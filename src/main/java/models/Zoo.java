92c7fbe0c4a3292c8535f629548017863cab020d
RonanBRUNETB22009596 <ronbrun15@gmail.com>
2023-12-04 22:51:16 +0100

    class Zoo

---
+++ src/main/java/models/Zoo.java
@@ -0,0 +1,66 @@
+package models;
+
+import models.enclosures.Enclosure;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.ArrayList;
+
+public class Zoo {
+
+    private String name;
+
+    private String fantasticMaster;
+
+    private int numberMaxEnclosure;
+
+    private ArrayList<Enclosure> enclosurelist;
+
+    public Zoo(String name, String fantasticMaster, int numberMaxEnclosure) {
+        this.name = name;
+        this.fantasticMaster = fantasticMaster;
+        this.fantasticMaster = fantasticMaster;
+        this.enclosurelist = new ArrayList<>();
+    }
+
+    public void displayNumberCreature(){
+        int totalCreature = 0;
+        for (Enclosure enclos : numberMaxEnclosure) {
+            System.out.println("Créature dans le Zoo fantastique'" + enclos.getName() + "':");
+            System.out.println();
+        }
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFantasticMaster() {
+        return fantasticMaster;
+    }
+
+    public void setFantasticMaster(String fantasticMaster) {
+        this.fantasticMaster = fantasticMaster;
+    }
+
+    public int getNumberMaxEnclosure() {
+        return numberMaxEnclosure;
+    }
+
+    public void setNumberMaxEnclosure(int numberMaxEnclosure) {
+        this.numberMaxEnclosure = numberMaxEnclosure;
+    }
+
+    public ArrayList<Enclosure> getEnclosurelist() {
+        return enclosurelist;
+    }
+
+    public void setEnclosurelist(ArrayList<Enclosure> enclosurelist) {
+        this.enclosurelist = enclosurelist;
+    }
+}
