954304ed0febef6b766c808ee5f51fe74f518163
ayoug <ayoub.essalhia@gmail.com>
2023-12-05 19:23:38 +0100

    started work on reproduction system for oviparous

--- src/main/java/models/creatures/Oviparous.java
+++ src/main/java/models/creatures/Oviparous.java
@@ -2,5 +2,22 @@ package models.creatures;
 
 public abstract class Oviparous extends Creature{
 
-    public void layEgg(){}
+    public boolean reproduce(Oviparous mate) {
+        if (canMateWith(mate)) {
+            Oviparous female;
+            Oviparous male;
+            if (this.getGender() == Gender.FEMALE) {
+                female = this;
+                male = mate;
+            } else {
+                female = mate;
+                male = this;
+            }
+            if (!female.getActualEnclosure().isFull())
+                return female.layEgg();
+        }
+        return false;
+    }
+
+    public boolean layEgg(){}
 }
